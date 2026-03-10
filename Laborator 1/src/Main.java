d23631aeaa202de11c6c5ce7e1d7fd1f0398a376
GabrielaGiorgiana <gabispatariu92@gmail.com>
2026-03-11 00:10:49 +0200

    Add files via upload

---
+++ Laborator 1/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Student student1 = new Student("A123", "Ion", "Popescu", "312AA");
+
+
+        System.out.println(student1);
+    }
+}
